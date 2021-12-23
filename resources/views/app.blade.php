271a045dfb99e9c7a5d101e7a1ff8684d2773cbc
Simon Rubuano <sr@mgk.dev>
2021-12-23 15:13:54 +0100

    feat: add favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,16 @@
 
         <title>Vortex</title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1d1c29">
+        <meta name="apple-mobile-web-app-title" content="Vortex">
+        <meta name="application-name" content="Vortex">
+        <meta name="msapplication-TileColor" content="#1d1c29">
+        <meta name="theme-color" content="#ffffff">
+
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
